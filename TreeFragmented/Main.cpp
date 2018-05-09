ea74bc48932bb9c28d28b64af6003c9ba9c69906
IlliaYaschenko <illyaillya2906@gmail.com>
2018-05-09 19:28:02 +0300

    second main

--- TreeFragmented/Main.cpp
+++ TreeFragmented/Main.cpp
@@ -5,41 +5,6 @@ using namespace std;
 
 int heighththt = 0;
 
-/*int main() {
-	Tree<int> tree;
-
-	tree.insert(50);
-	tree.insert(100);
-	tree.insert(125);
-	tree.insert(150);
-	tree.insert(75);
-	tree.insert(111);
-	tree.insert(70);
-	tree.insert(95);
-	tree.insert(25);
-	tree.insert(21);
-	tree.insert(45);
-	tree.insert(27);
-	tree.insert(51);
-	//tree.watch();
-	//cout << tree.height() << endl;
-	//tree.deleteItem(2);
-	//tree.printCount(3);
-	tree.watch();
-	tree.deleteItem(75);
-	tree.watch();
-	tree.deleteItem(50);
-	//tree.deleteItem(100);
-	tree.watch();
-	//cout << tree.find(2);
-	//cout<< tree.FindWay(70)<<endl;
-	//cout << tree.findToPrint(2) << endl;
-	cout << "x";
-
-	system("pause");
-}*/
-
-// main for problems
 int main() {
 	Tree<int> tree;
 	// random elements in tree
